d84b7f8c6936abec430e1be133d8e555f9e31839
Serina Tsang <serinat@uw.edu>
2019-10-10 02:20:17 -0700

    created questions

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "What is my middle name?",
+        choices: ["Mei", "Williams", "China", "I don't have one!"],
+        answer: "I don't have one!"
+    },
+    {
+        title: "What is my nationality?",
+        choices: ["Chinese", "Filipino", "Chinese-American", "Russian"],
+        answer: "Chinese-American"
+    },
+    {
+        title: "How many pets do I currently own?",
+        choices: ["None", "2, doxiepoo and frenchie", "2, both guinea pigs", "4, doxiepoo, frenchie & 2 guinea pigs"],
+        answer: "4 doxiepoo, frenchie & 2 guinea pigs"
+    },
+    {
+        title: "What is my favorite fast-food restaurant?",
+        choices: ["Chick Fil A", "Burger King", "McDonalds", "I don't do fast-food"],
+        answer: "McDonalds"
+    },
+    {
+        title: "Where is my favorite place to be, in order from most favorite to least favorite?",
+        choices: ["Hawaii, LA, Home, Any place with food", "Any place with food, Home, Hawaii, LA", "LA, Hawaii, Any place with food, Home", "Home, Any place with food, Hawaii, LA"],
+        answer: "Home, Any place with food, Hawaii, LA"
+    },
+];
