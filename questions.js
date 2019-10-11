1f92bbeccf16f400886ee0895d5f12b2588401e9
Serina Tsang <serinat@uw.edu>
2019-10-11 16:15:57 -0700

    finalized questions and answers

--- questions.js
+++ questions.js
@@ -1,27 +1,37 @@
+//track # of correct answers
+var score = 0;
+//keeping track of questions
+var currentQuestion = 0;
+//store questions in an array
 var questions = [
     {
-        title: "What is my middle name?",
+        question: "What is my middle name?",
         choices: ["Mei", "Williams", "China", "I don't have one!"],
         answer: "I don't have one!"
     },
     {
-        title: "What is my nationality?",
+        question: "What is my nationality?",
         choices: ["Chinese", "Filipino", "Chinese-American", "Russian"],
         answer: "Chinese-American"
     },
     {
-        title: "How many pets do I currently own?",
+        question: "How many pets do I currently own?",
         choices: ["None", "2, doxiepoo and frenchie", "2, both guinea pigs", "4, doxiepoo, frenchie & 2 guinea pigs"],
         answer: "4 doxiepoo, frenchie & 2 guinea pigs"
     },
     {
-        title: "What is my favorite fast-food restaurant?",
+        question: "What is my favorite fast-food restaurant?",
         choices: ["Chick Fil A", "Burger King", "McDonalds", "I don't do fast-food"],
         answer: "McDonalds"
     },
     {
-        title: "Where is my favorite place to be, in order from most favorite to least favorite?",
+        question: "Where is my favorite place to be, in order from most favorite to least favorite?",
         choices: ["Hawaii, LA, Home, Any place with food", "Any place with food, Home, Hawaii, LA", "LA, Hawaii, Any place with food, Home", "Home, Any place with food, Hawaii, LA"],
         answer: "Home, Any place with food, Hawaii, LA"
     },
 ];
+
+//ensures code will run once the entire page & DOM are ready
+$(document).ready(function () {
+
+}
