f0b53d6e60ea288249ef46eec9d44af8185049af
Serina Tsang <serinat@uw.edu>
2019-10-12 23:09:27 -0700

    added functionality

--- questions.js
+++ questions.js
@@ -5,29 +5,29 @@ var currentQuestion = 0;
 //store questions in an array
 var questions = [
     {
-        question: "What is my middle name?",
-        choices: ["Mei", "Williams", "China", "I don't have one!"],
-        answer: "I don't have one!"
+        title: "What is my middle name?",
+        choices: ["Mei", "Williams", "China", "I don't offically have one!"],
+        answer: 3
     },
     {
-        question: "What is my nationality?",
+        title: "What is my nationality?",
         choices: ["Chinese", "Filipino", "Chinese-American", "Russian"],
-        answer: "Chinese-American"
+        answer: 2
     },
     {
-        question: "How many pets do I currently own?",
-        choices: ["None", "2, doxiepoo and frenchie", "2, both guinea pigs", "4, doxiepoo, frenchie & 2 guinea pigs"],
-        answer: "4 doxiepoo, frenchie & 2 guinea pigs"
+        title: "How many pets do I currently own?",
+        choices: ["None", "2: doxiepoo and frenchie", "2: both guinea pigs", "4: doxiepoo, frenchie & 2 guinea pigs"],
+        answer: 3
     },
     {
-        question: "What is my favorite fast-food restaurant?",
+        title: "What is my favorite fast-food restaurant?",
         choices: ["Chick Fil A", "Burger King", "McDonalds", "I don't do fast-food"],
-        answer: "McDonalds"
+        answer: 2
     },
     {
-        question: "Where is my favorite place to be, in order from most favorite to least favorite?",
-        choices: ["Hawaii, LA, Home, Any place with food", "Any place with food, Home, Hawaii, LA", "LA, Hawaii, Any place with food, Home", "Home, Any place with food, Hawaii, LA"],
-        answer: "Home, Any place with food, Hawaii, LA"
+        title: "Where is my favorite place to be, in order from most favorite to least favorite?",
+        choices: ["Hawaii, LA, Home, Any place with good food", "Any place with good food, Home, Hawaii, LA", "LA, Hawaii, Any place with good food", "Home, Any place with good food, Hawaii, LA"],
+        answer: 3
     },
 ];
 
@@ -49,3 +49,100 @@ $(document).ready(function () {
     }
     );
 });
+
+//add event listener on user answer
+$(".quiz ul").on("click", "li", function (event) {
+    event.preventDefault();
+    $(".selected").removeClass("selected");
+    $(this).addClass("selected");
+    if ($("li.selected").length) {
+        var guess = parseInt($("li.selected").attr("id"));
+        checkAnswer(guess);
+    }
+});
+
+//add event listener on submit button to store users initials & score
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+
+    var initials = document.querySelector("#initial").value;
+
+    if (initials === "") {
+        displayMessage("error", "Initial cannot be blank");
+    } else {
+        localStorage.setItem("initial", initials);
+    }
+
+    $(".view-scores").append("<li>" + initials + " : " + score + "</li>");
+    displayScore();
+})
+
+//add event listener to try again button to restart quiz
+$("#restart").on("click", function (event) {
+    event.preventDefault();
+    restart();
+});
+
+//add event listener on clear scoreboard button
+$("#clear").on("click", function (event) {
+    $(".view-scores li").html("");
+});
+
+$(".view-scores").on("click", function (event) {
+    displayScore();
+})
+
+//function to display one question at a time
+function displayQuestion() {
+    var question = questions[currentQuestion];
+    $(".quiz h3").text(question.title);
+    $(".quiz ul").html("");
+    for (var i = 0; i < question.choices.length; i++) {
+        $(".quiz ul").append("<li id = '" + i + "'> " + question.choices[i] + "</li>");
+    };
+}
+
+//function to check users answers
+function checkAnswer(guess) {
+    var question = questions[currentQuestion];
+    if (question.answer === guess) {
+        score++;
+        $("#alert").append("Correct!" + "<br>");
+
+    } else {
+        $("#alert").append("Wrong!" + "<br>");
+
+    }
+    currentQuestion++;
+    if (currentQuestion >= questions.length) {
+        showResults();
+    } else {
+        displayQuestion();
+    };
+}
+//function to show results
+function showResults() {
+    $(".quiz").hide();
+    $(".results").show();
+    $(".results p").text("Yay! You scored " + score + " out of " + questions.length + " correct!")
+}
+
+//function to display scores
+function displayScore() {
+    $(".results").hide();
+    $(".quiz").hide();
+    $(".final").show();
+    
+
+}
+//function to restart quiz
+function restart() {
+    $(".results").hide();
+    $(".final").hide();
+    $(".quiz").show();
+    score = 0;
+    currentQuestion = 0;
+    $("#alert").html("");
+    $("#initial").html("");
+    displayQuestion();
+}
